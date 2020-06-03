71d6e6cb86fb647ba61a278f83df6e426df753f0
George-Ogden <G02287@outlook.com>
2020-06-03 15:17:21 +0100

    Modified code

--- main/minifier.py
+++ main/minifier.py
@@ -3,7 +3,7 @@ import boto3
 import requests
 urls = {"html":'https://html-minifier.com/raw',"css":"https://cssminifier.com/raw","js":"https://javascript-minifier.com/raw"}
 client = boto3.client("s3")
-def lambda_handler(event, context=None):
+def handler(event, context=None):
     files = map((lambda record: {"Key":record["s3"]["object"]["key"],"Bucket":record["s3"]["bucket"]["name"]}),event["Records"])
     for location in files:
         file = client.get_object(**location)["Body"]
@@ -12,5 +12,8 @@ def lambda_handler(event, context=None):
         if not extension in urls.keys(): return 
         url = urls[extension]
         request = requests.post(url , {'input': file._raw_stream})
+        if location["Key"] != "index.html" and extension == "html": 
+            client.delete_object(**location)
+            location["Key"] = ".".join(location["Key"].split(".")[:-1])
         client.put_object(**location,Body=request.text)
     return {"statusCode":200}
